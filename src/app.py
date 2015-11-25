c30761bcae0a12c7354e203ee7339f96a68bce8c
dchapp <dchapp@udel.edu>
2015-11-24 23:04:15 -0500

    implemented part of hei scoring function

--- src/app.py
+++ src/app.py
@@ -7,6 +7,25 @@ import glob
 import os
 import openpyxl
 
+"""
+nut_data = {iff: , tot: }
+dem_data = {age: , sex: , income: , edu: }
+"""
+def respondent(object):
+    def __init__(self, sequence_num, year):
+        self.sn = sequence_num
+        self.yr = year
+        self.hei_scores = {}
+        self.nut_data = {}
+        self.dem_data = {}
+
+    def assign_hei_score(self):
+
+    def populate_dem_data(self):
+
+    def populate_nut_data(self):
+        
+
 
 def ingest_iff_file(iff_file):
     records = {}
@@ -15,6 +34,14 @@ def ingest_iff_file(iff_file):
         for line in iff_file:
             print line.split()
             break
+
+def ingest_tot_file(tot_file):
+
+
+
+def ingest_dem_file(dem_file):
+
+
 
 ### Load in all files (currently just for 1 year)
 iff_files = {}
--- src/score.py
+++ src/score.py
@@ -48,15 +48,126 @@ def hei_score(dietary_recall):
     return total_score         
 
 
+class Respondent(object):
+    def __init__(self, sequence_number, year):
+        self.sn = sequence_number
+        self.yr = year
+        self.iff_records = []
+        self.tot_record = []
+        self.dem_record = []
+        self.dem_data = {'age': 0, 'sex': 0, 'edu': 0, 'income': 0}
+        self.hei = 0
+    
+    def extract_food_codes(self):
+        food_codes = []
+        for r in self.iff_records:
+            food_codes.append(int(r[15]))
+        return food_codes
 
-import openpyxl
+    def compute_hei(self):
+
+        ### Compute adequacy components
+        food_codes = self.extract_food_codes()
+        ### Categorize 
+
+        ### Compute fatty acid component
+        pufas = float(self.tot_record[18])
+        mufas = float(self.tot_record[17])
+        sfas  = float(self.tot_record[16])
+        fatty_acid_score = (pufas + mufas) / sfas
+        fatty_acid_points = min((fatty_acid_score - 1.2) * (10/(2.5 - 1.2)), 10)
+            
+        ### Compute moderation components
+
+
+        print pufas
+        print mufas
+        print sfas
+        print fatty_acid_score
+        print fatty_acid_points
+        print food_codes
+
+    
+
 import sys
 
+respondents = {}
 
 
 """
-with open(sys.argv[1]) as data:
-    for record in data:
-        print record.split('    ')
+Do a pass through the individual foods file
 """
+with open(sys.argv[1]) as iff_data:
+    year = 1999
+    iff_headers = iff_data.next().split(',')
+    #print len(iff_headers)
+    for line in iff_data:
+        record = line.split(',') 
+        sequence_number = int(record[0])
+        
+        #print record
+        #print len(record)
+
+        ### If this is the first time encountering the current sequence number,
+        ### create a new respondent object. If the sequence number has been seen
+        ### before, accumulate relevant data into existing respondent object
+        if sequence_number not in respondents:
+            ### Create Respondent instance
+            r = Respondent(sequence_number, year)
+            ### Add current line to r's list of raw iff records
+            r.iff_records.append(record)
+            ### Insert Respondent instance into dict of all respondents under
+            ### its sequence number
+            respondents[sequence_number] = r 
+        else:
+            ### Add current line to r's list of raw records
+            respondents[sequence_number].iff_records.append(record)
+
+
+"""
+Now do a pass through the dietary totals file 
+WARNING: There are sequence numbers in here that AREN'T in the IFF file...
+"""
+with open(sys.argv[2]) as tot_data:
+    tot_headers = tot_data.next().split(',')
+    for line in tot_data:
+        record = line.split(',')
+        sequence_number = int(record[0])
+        if sequence_number in respondents:
+            respondents[sequence_number].tot_record = record
+
+
+"""
+Now do a pass through the demographic data file
+"""
+with open(sys.argv[3]) as dem_data:
+    dem_headers = dem_data.next().split(',')
+    for line in dem_data:
+        record = line.split(',')
+        sequence_number = int(record[0])
+        if sequence_number in respondents:
+            respondents[sequence_number].dem_record = record
+
+
+        
+print respondents[1].sn
+print respondents[1].yr
+#print respondents[1].iff_records
+print respondents[1].dem_data
+#print respondents[1].extract_food_codes()
+
+print "DRXTOT DATA"
+print respondents[1].tot_record
+
+print "DEMO DATA"
+print respondents[1].dem_record
+         
+        
+print "FOOD CODES / HEI STUFF"
+respondents[1].compute_hei()
+
+
+
+
+
 
